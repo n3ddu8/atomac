171f30cda215dd3f88af23f7410aad3180354355
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-10-23 14:59:38 +0100

    fix: added root user to nixbld group

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,8 @@ dnf -y install evolution
 curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-arm64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
 mkdir -m 0755 /nix && chown root /nix
 groupadd nixbld
-echo $USER
+usermod -aG nixbld root
+whoami
 curl -L https://nixos.org/nix/install | sh
 # . ~/.nix-profile/etc/profile.d/nix.sh
 
