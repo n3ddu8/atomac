a2ed0d270adf01e9fc04061501ce687e17b5e93e
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-10-26 09:06:23 +0000

    feat: added a writable nix directory

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,28 +2,12 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
 dnf -y install evolution
 
-curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-arm64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
-
-# curl -L https://nixos.org/nix/install | sh
-# . ~/.nix-profile/etc/profile.d/nix.sh
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-arm64" \
+  && install -c -m 0755 devpod /usr/local/bin \
+  && rm -f devpod
 
 systemctl enable podman.socket
+systemctl enable nix.mount
+
