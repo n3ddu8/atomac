1de1a2ac8f46407c9a3a6fb0906a074dad7007df
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-10-30 21:25:58 +0000

    feat: added xremap and gnome macos keybindings

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,12 +2,25 @@
 
 set -ouex pipefail
 
-dnf -y install chezmoi
-dnf -y install python-pip
+dnf -y install \
+  chezmoi \
+  python-pip
 
+# devpod CLI for ARM
 curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-arm64" \
   && install -c -m 0755 devpod /usr/local/bin \
   && rm -f devpod
+
+# gnome macos keybindings
+wget https://github.com/xremap/xremap/releases/latest/download/xremap-linux-aarch64-gnome.zip \
+  && unzip -o ./xremap-linux-aarch64-gnome.zip \
+  && cp ./xremap /usr/local/bin \
+  && mkdir -p $HOME/.config/gnome-macos-remap \
+  && cp gnome-macos-remap.yml $HOME/.config/gnome-macos-remap/config.yml \
+  && mkdir -p $HOME/.local/share/systemd/user \
+  && cp gnome-macos-remap.service $HOME/.local/share/systemd/user \
+  && cp bin/*.sh $HOME/.local/bin \
+  && systemctl enable gnome-macos-remap
 
 systemctl enable podman.socket
 
